ad9e33e452f36d509a607e6dbf917a88cb107be1
Ayoub Kremcht <nowkreator@gmail.com>
2021-09-05 23:45:01 +0100

    init package

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# zoom_tap_animation
-Zoom Tap Animation is is a package that allows you to make an zoom animation effect similar to App Store's (ios app) items in Today tab, when you long tap on some item, it start to zoom in then it zoom out when you release it.
+# Zoom Tap Animation
+
+Zoom Tap Animation is is a package that allows you to make an zoom animation effect similar to App Store's (ios app) items in Today tab, when you long tap on some item, it start to zoom in then it zoom out when you release it..
+
+## How To Use It:
+
+# add the package to your dependencies
+
+```dart
+dependencies:
+  zoom_tap_animation: ^1.0.0
+```
+
+```dart
+import 'package:zoom_tap_animation/zoom_tap_animation.dart';
+```
+
+# use ZoomTapAnimation widget
+
+```dart
+ZoomTapAnimation(
+      onTap: (){
+            // what should happen when you tap on the widget
+      },
+            // put your widget here
+      child: YOUR_WIDGET
+);
+```
+
+## Parameters
+| parameter       | description                                              | default                           |
+|-----------------|----------------------------------------------------------|-----------------------------------|
+| child           | your child widget that you want put the effect on.       |                                   |
+| onTap           | what should happen if you tap on the widget.             | null                              |
+| begin           | the size of widget you want to begin with.               | 1.0                               |
+| end             | the size of widget you want to begin with.               | 0.93                              |
+| duration        | the duration of the begin zoom in animation.             | const Duration(milliseconds: 20)  |
+| reverseDuration | the duration of the end zoom in animation.               | const Duration(milliseconds: 120) |
+| beginCurve      | the curve animation type of the begin zoom in animation. | Curves.decelerate                 |
+| endCurve        | the curve animation type of the end zoom in animation.   | Curves.fastOutSlowIn              |
---
+++ lib/zoom_tap_animation.dart
@@ -0,0 +1,70 @@
+library zoom_tap_animation;
+
+import 'dart:async';
+import 'package:flutter/widgets.dart';
+
+class ZoomTapAnimation extends StatefulWidget {
+  final Widget child;
+  final double begin, end;
+  final Duration duration, reverseDuration;
+  final Function()? onTap;
+  final Curve beginCurve, endCurve;
+  const ZoomTapAnimation(
+      {Key? key,
+      required this.child,
+      this.onTap,
+      this.begin = 1.0,
+      this.end = 0.93,
+      this.duration = const Duration(milliseconds: 20),
+      this.reverseDuration = const Duration(milliseconds: 120),
+      this.beginCurve = Curves.decelerate,
+      this.endCurve = Curves.fastOutSlowIn})
+      : super(key: key);
+  @override
+  State<StatefulWidget> createState() => _ZoomTapAnimationState();
+}
+
+class _ZoomTapAnimationState extends State<ZoomTapAnimation> with SingleTickerProviderStateMixin<ZoomTapAnimation> {
+  AnimationController? _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: widget.reverseDuration, value: 1.0, reverseDuration: widget.duration);
+    _animation = Tween(begin: widget.end, end: widget.begin)
+        .animate(CurvedAnimation(parent: _controller!, curve: widget.beginCurve, reverseCurve: widget.endCurve));
+    _controller?.forward();
+  }
+
+  bool _isOnTap = false;
+  @override
+  Widget build(BuildContext context) {
+    return Listener(
+        onPointerDown: (c) async {
+          _isOnTap = true;
+          _controller?.reverse();
+          await Future.delayed(const Duration(milliseconds: 150), () async {
+            if (!_isOnTap) widget.onTap?.call();
+          });
+        },
+        onPointerMove: (c) async {
+          await Future.delayed(const Duration(milliseconds: 100), () {
+            _isOnTap = true;
+          });
+        },
+        onPointerUp: (c) async {
+          _isOnTap = false;
+          await _controller?.forward();
+        },
+        child: ScaleTransition(scale: _animation, child: widget.child));
+  }
+
+  @override
+  void dispose() {
+    _controller?.stop();
+    _controller?.dispose();
+    _controller = null;
+    super.dispose();
+  }
+}
